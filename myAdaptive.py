803be0ba301b9a5fa74cd512e393338525396651
Ronald Adomako <ronaldadomako@icloud.com>
2018-09-27 13:15:20 -0400

    transform data to ascii

--- myAdaptive.py
+++ myAdaptive.py
@@ -1,1 +1,19 @@
-import sqlite
+import sqlite3, string
+conn = sqlite3.connect('population.db')
+
+c = conn.cursor()
+c.execute('''DROP TABLE IF EXISTS census''')
+c.execute('''CREATE TABLE census (geography text, base int, estimate int, year0 int, year1 int, year2 int, year3 int, year4 int, year5 int, year6 int, year7 int)''')
+
+pop = open('population.txt','r',encoding='ascii',errors='surrogateescape')
+count = 0
+for line in pop:
+    count+=1
+    print(line.strip())
+    if count == 100:
+        break
+
+print(count)
+    
+pop.close()
+conn.close()
